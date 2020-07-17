661063a05a24f9db02b2c1a08e6db26206f2c5fe
Taylor Somers <tsomers@yandex.com>
2020-07-17 14:15:21 -0400

    Add UI logic to query whether user would like to make an order and to display prices for bread and pastries

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,32 +6,39 @@ class Program
 {
   static void Main()
   {
-    Console.WriteLine("Welcome to Pierre's Bakery! We are very happy to serve you today. At Pierre's we offer bread loaves and pastries, both freshly made to order. To get started, please enter the number of bread loaves you would like to order: ");
-    string breadString = Console.ReadLine();
-    int breadAmount = int.Parse(breadString);
-    List<int> orderList = new List<int> {};
-    Bread bread = new Bread(breadAmount);
-    if (bread.OrderAmount < 0)
+    Console.WriteLine("Welcome to Pierre's Bakery! We are very happy to serve you today. At Pierre's we offer bread loaves and pastries, both freshly made to order. Would you like to place an order today? Type Y for Yes or N for No.");
+    if (Console.ReadLine().ToLower() == "n")
     {
-      Console.WriteLine("We cannot process orders for negative amounts of bread. Perhaps you should consult a theoretical physicist about such a request?");
+      Console.WriteLine("Goodbye.");
     }
     else
     {
-      orderList.Add(breadAmount);
+      Console.WriteLine("Great! Our price per loaf of bread is $5. For every two loaves you order, the third is free! Pastries are $2 per item, and $1 off for every third pastry you order. To get started, please tell us how many loaves of bread you would like to order: ");
+      string breadString = Console.ReadLine();
+      int breadAmount = int.Parse(breadString);
+      List<int> orderList = new List<int> {};
+      Bread bread = new Bread(breadAmount);
+      if (bread.OrderAmount < 0)
+      {
+        Console.WriteLine("We cannot process orders for negative amounts of bread. Perhaps you should consult a theoretical physicist about such a request?");
+      }
+      else
+      {
+        orderList.Add(breadAmount);
+      }
+      Console.WriteLine("Great! Now please enter the number of pastries you would like to order: ");
+      string pastryString = Console.ReadLine();
+      int pastryAmount = int.Parse(pastryString);
+      Pastry pastry = new Pastry(pastryAmount);
+      if (pastry.OrderAmount < 0)
+      {
+        Console.WriteLine("We cannot process orders for negative numbers of pastries. Perhaps you should consult a theoretical physicist about such a request?");
+      }
+      else
+      {
+        orderList.Add(pastryAmount);
+      }
+      Console.WriteLine(orderList[1]);
     }
-    Console.WriteLine("Great! Now please enter the number of pastries you would like to order: ");
-    string pastryString = Console.ReadLine();
-    int pastryAmount = int.Parse(pastryString);
-    Pastry pastry = new Pastry(pastryAmount);
-    if (pastry.OrderAmount < 0)
-    {
-      Console.WriteLine("We cannot process orders for negative numbers of pastries. Perhaps you should consult a theoretical physicist about such a request?");
-    }
-    else
-    {
-      orderList.Add(pastryAmount);
-    }
-    Console.WriteLine(orderList[1]);
-    Main();
   }
 }
