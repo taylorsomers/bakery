3fde7d9adf9deb683c2f1e4ca462a9d6d499245e
Taylor Somers <tsomers@yandex.com>
2020-07-17 14:00:23 -0400

    Add UI logic to collect number of pastries the user would like to order

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -19,7 +19,19 @@ class Program
     {
       orderList.Add(breadAmount);
     }
-    Console.WriteLine(breadAmount);
+    Console.WriteLine("Great! Now please enter the number of pastries you would like to order: ");
+    string pastryString = Console.ReadLine();
+    int pastryAmount = int.Parse(pastryString);
+    Pastry pastry = new Pastry(pastryAmount);
+    if (pastry.OrderAmount < 0)
+    {
+      Console.WriteLine("We cannot process orders for negative numbers of pastries. Perhaps you should consult a theoretical physicist about such a request?");
+    }
+    else
+    {
+      orderList.Add(pastryAmount);
+    }
+    Console.WriteLine(orderList[1]);
     Main();
   }
 }
