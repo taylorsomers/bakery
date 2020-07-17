2dde8faa925f10d30fd57ad8cc76a8e1ad57e633
Taylor Somers <tsomers@yandex.com>
2020-07-17 11:21:44 -0400

    Initial commit

---
+++ Bakery.Tests/ModelTests/BusinessLogicTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NamespaceName;
+
+namespace NewProject.Tests
+{
+  [TestClass]
+  public class BusinessLogicTests
+  {
+    [TestMethod]
+    // Test method will go here
+  }
+}
---
+++ Bakery/Models/BusinessLogic.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic; //remove if not using Dictionary in app //
+
+namespace NamespaceName
+{
+  class ClassName
+  {
+    
+  }
+}
