49150d962ae7f267c1aad11cf3d00294fb989bfb
Taylor Somers <tsomers@yandex.com>
2020-07-17 12:18:29 -0400

    Correct namespace and class names and write failing test for Bread constructor

--- Bakery.Tests/ModelTests/Bakery.Tests.cs
+++ Bakery.Tests/ModelTests/Bakery.Tests.cs
@@ -1,12 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NamespaceName;
+using BakedGoods;
 
-namespace NewProject.Tests
+namespace BakedGoods.Tests
 {
   [TestClass]
-  public class BusinessLogicTests
+  public class BreadTests
   {
     [TestMethod]
-    // Test method will go here
+    public void Bread_CreateBreadOrderObject_BreadOrderObject()
+    {
+    Bread bread = new Bread(1);
+    Assert.AreEqual(1, bread.OrderAmount);
+    }
   }
 }
